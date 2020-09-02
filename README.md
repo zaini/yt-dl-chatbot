baa464413f192157c2649a65f8026511b97328be
Zaini <alimzaini@outlook.com>
2020-09-02 20:22:01 +0100

    Updated README and reformatting

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # yt-dl-chatbot
-YouTube download chatbots for Telegram and WhatsApp
+YouTube download chatbots for Telegram (and maybe WhatsApp soon)
+
+# Installation & Setup
+
+# Usage
+
+# Other
+
+## Some of the libraries used
+
+* https://github.com/python-telegram-bot/python-telegram-bot
--- main.py
+++ main.py
@@ -5,13 +5,16 @@ load_dotenv()
 
 TOKEN = os.getenv('TOKEN')
 
+
 def hello(update, context):
     update.message.reply_text(
         'Hello {}'.format(update.message.from_user.first_name))
 
+
 def help(update, context):
     update.message.reply_text(
         'Helping {}'.format(update.message.from_user.first_name))
+
 
 updater = Updater(TOKEN, use_context=True)
 
